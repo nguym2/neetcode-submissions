e609ff1c0c577c9c780bd7fae07e50ebbea27c97
nguym2 <98132018+nguym2@users.noreply.github.com>
2026-09-08 23:37:32 -0500

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length != t.length) {
+            return false;
+        }
+
+        const charMap = {};
+
+        for (let i = 0; i < s.length; i++) {
+            charMap[s[i]] = (charMap[s[i]] ?? 0) + 1;
+            charMap[t[i]] = (charMap[t[i]] ?? 0) - 1;
+        }
+
+        return Object.values(charMap).every((val) => val === 0);
+    }
+}
